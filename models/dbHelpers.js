6b5cdaa7e2921d06d51161441db81548aed360e9
Raul Aviles <finbravilesr@gmail.com>
2020-12-19 08:53:45 -0500

    se insertó pg

--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -16,9 +16,9 @@ module.exports = {
 };
 
 async function add(lesson) {
-   const [id] = await db('lessons').insert(lesson);
-
-   return findById(id);
+   return await db('lessons').insert(lesson, ['id', 'name'])
+   //const [id] = await db('lessons').insert(lesson);
+   //return findById(id);
 }
 
 function find(){
@@ -54,10 +54,13 @@ function findMessageById(id){
 }
 
 async function addMessage(message, lesson_id){
-    const [id] = await db('messages')
+    return await db('messages')
     .where({lesson_id})
-    .insert(message);
-    return findMessageById(id);
+    .insert(message, ['id'])
+    //const [id] = await db('messages')
+    //.where({lesson_id})
+    //.insert(message);
+    //return findMessageById(id);
 }
 
 function findLessonMessages(lesson_id){
